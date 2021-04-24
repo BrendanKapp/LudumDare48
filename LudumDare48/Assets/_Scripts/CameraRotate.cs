41c235d27833476746fe7a3bf6f9a2a37bd42eab
Brendan Kapp <brendaniskapp@gmail.com>
2021-04-24 01:21:24 -0400

    Add camera tilt

---
+++ LudumDare48/Assets/_Scripts/CameraRotate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float maxRotation = 5;
+    [SerializeField]
+    private float rotationIncrement = 0.1f;
+    [SerializeField]
+    private float haltAngle = 0.1f;
+    private float currentRotation;
+    private void Update()
+    {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        //transform.localRotation = Quaternion.Euler(0, 0, horizontal * maxRotation);
+        //transform.rotation = Quaternion.Euler(0, 0, horizontal * cameraRotateMultiplier);
+        if (horizontal > 0)
+        {
+            if (currentRotation > -maxRotation) Rotate(-1);
+        } else if (horizontal < 0) {
+            if (currentRotation < maxRotation) Rotate(1);
+        } else {
+            if (currentRotation < -haltAngle)
+            {
+                Rotate(1);
+            }
+            else if (currentRotation > haltAngle)
+            {
+                Rotate(-1);
+            } else {
+                transform.localRotation = Quaternion.identity;
+                currentRotation = 0;
+            }
+        }
+    }
+    private void Rotate(int direction)
+    {
+        transform.Rotate(0, 0, direction * rotationIncrement * Time.deltaTime);
+        currentRotation += direction * rotationIncrement * Time.deltaTime; 
+    }
+}
